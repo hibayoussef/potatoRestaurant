fa6f52e3d3dc9cd5e25043135658e901e5ea2465
hiba.youssef <hiba.amar.youssef@gmail.com>
2023-11-09 14:07:53 +0200

    [ADD] add header section

--- src/components/SubHeading/SubHeading.jsx
+++ src/components/SubHeading/SubHeading.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { images } from '../../constants';
 
-const SubHeading = () => (
-  <div>
-    SubHeading
+const SubHeading = ({ title }) => (
+  <div className='p__cormorant'>
+    <p>
+      {title}
+    </p>
+
+    <img src={images.spoon} alt='spoon' className='spoon__img' />
   </div>
 );
 
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-
+import { images } from '../../constants';
 import './Header.css';
+import { SubHeading } from '../../components';
 
 const Header = () => (
-  <div>
-    Header
+  <div className='app__header app__wrapper section__padding' id='home'>
+    <div className='app__header_info'>
+      <SubHeading title="Potato fun in a new way" />
+      <h1 className='app__header-h1'>The key to eating delicious potatoes</h1>
+      <p className='p__opensans' style={{ margin: '2rem 0' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores laudantium enim perferendis ipsum distinctio nemo consectetur corrupti perspiciatis ex minus, consequuntur exercitationem excepturi delectus odio quo, blanditiis corporis possimus aut.</p>
+      <button type="button" className='custom__button'>Explore More</button>
+    </div>
+
+    <div className='app__header_img'>
+      <img src={images.welcome} alt='welcom' />
+    </div>
   </div>
 );
 
